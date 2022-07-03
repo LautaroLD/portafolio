b075d4403d2fa64565c0cc8eb60964a39a991ebf
lautaroLD <lauttyd@gmail.com>
2022-07-03 20:26:24 -0300

    correccion detalles

--- code.js
+++ code.js
@@ -1,5 +1,3 @@
-import { registerElement } from "./Observer.js"
-
 function pushElement(classElem, name, sitio, repo, imageBg) {
     let cont = document.getElementById('list-proyects-container')
     let col = Object.assign(document.createElement('DIV'), {
@@ -25,34 +23,23 @@ function pushElement(classElem, name, sitio, repo, imageBg) {
         className: `card-footer`,
     })
 
-    // let contentText = Object.assign(document.createElement('DIV'), {
-    //     className: 'contentText',
-    // })
-
-
     let visitBtn = Object.assign(document.createElement('A'), {
         className: 'btn btn-primary',
-        // id: 'visit-btn',
         textContent: 'Visitar',
         href: sitio,
         target: '_blank'
     })
     let repoBtn = Object.assign(document.createElement('A'), {
         className: 'btn btn-link',
-        // id: 'repo-btn',
         textContent: 'Repositorio',
         href: repo,
         target: '_blank'
     })
-    // contentText.append(title, visitBtn, repoBtn)
-    // div.append(img, contentText)
-    // cont.appendChild(div)
     cardBody.append(title)
     cardFooter.append(visitBtn, repoBtn)
     card.append(img, cardBody, cardFooter)
     col.append(card)
     cont.append(col)
-    // registerElement(div)
 }
 let list = []
 fetch(`/assets/proyects.json`)
@@ -63,30 +50,3 @@ fetch(`/assets/proyects.json`)
 
     })
     .catch((err) => new Error(err))
-// elementsVisible()
-
-// function btnsVisible() {
-//     let btns = document.getElementsByClassName('btns')
-//     let listBtns = []
-//     listBtns = [...btns]
-
-//     listBtns.forEach(elem => {
-//         let position = elem.parentElement.getBoundingClientRect()
-//         if (position.bottom < (screen.height - (screen.height / 5)) && position.bottom > 0) {
-//             elem.classList.replace('proyects-btns-none', 'proyects-btns')
-//         }
-//     })
-// }
-// function seeAboutMeSection() {
-//     let aboutMe = document.getElementById('about-me-container')
-//     let position = aboutMe.getBoundingClientRect()
-//     if (position.top < screen.height) {
-//         aboutMe.classList.add('see-aboutMe')
-//     }
-// }
-// function elementsVisible() {
-//     document.addEventListener('scroll', () => {
-//         // btnsVisible()
-//         seeAboutMeSection()
-//     })
-// }
