6877e68c884020eab5550b0509e8ca300d802c4c
LautaroLD <84945392+LautaroLD@users.noreply.github.com>
2022-12-30 22:06:04 -0300

    corrijo error en navegacion

--- src/components/screens/MessageSendScreen.jsx
+++ src/components/screens/MessageSendScreen.jsx
@@ -4,7 +4,7 @@ function MessageSendScreen() {
   return (
     <>
       <h3>Gracias por su mensaje, pronto recibirá una respuesta.</h3>
-      <a className='btn btn-repo' href={`${document.location.origin}`}>volver</a>
+      <a className='btn btn-repo' href={`${document.location.origin}${document.location.pathname}`}>volver</a>
     </>
   )
 }
