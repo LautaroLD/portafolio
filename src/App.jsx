dbfa261c88c3dab8c349e8f8ac64c94ab718af16
LautaroLD <duranlautarogabriel@gmail.com>
2023-11-04 20:02:46 -0300

    fix route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React, { lazy, Suspense } from 'react';
-import MessageSendScreen from './screens/MessageSendScreen';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import Loading from './assets/Spinner-loading.svg'
 
 const HomeScreen = lazy(() => import('./screens/HomeScreen'))
+const MessageSendScreen = lazy(() => import('./screens/MessageSendScreen'))
 function App() {
 	return (
 		<Suspense fallback={<img src={Loading} width={250} height={250} className='m-auto' />}>
