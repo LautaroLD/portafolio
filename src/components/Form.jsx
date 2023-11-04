964f7f57ef91cde6ee5387851d5752105e98979f
LautaroLD <duranlautarogabriel@gmail.com>
2023-11-04 20:39:18 -0300

    form update

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,9 +5,6 @@ function Form() {
         <form className='form' name='form-contacto' netlify>
             <input className='input' placeholder='Nombre' type="text" name="name" required />
             <input className='input' placeholder='Email' type="email" name="email" required />
-            <input type="hidden" name="_template" value="table"></input>
-            <input type="hidden" name="_captcha" value="false"></input>
-            <input type="hidden" name="_next" value={`${document.location.href}/send`}></input>
             <textarea className='input' placeholder='Mensaje' name="content" cols="30" rows="5" required />
             <button type="submit">Enviar</button>
         </form>
