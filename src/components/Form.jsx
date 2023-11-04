24b8f09676b43ba5859a35074601493d88131f5a
LautaroLD <duranlautarogabriel@gmail.com>
2023-11-04 20:51:31 -0300

    form update

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Form() {
     return (
-        <form className='form' name='form-contacto' netlify>
+        <form className='form' name='form-contacto' netlify method='POST'>
             <input className='input' placeholder='Nombre' type="text" name="name" required />
             <input className='input' placeholder='Email' type="email" name="email" required />
             <textarea className='input' placeholder='Mensaje' name="content" cols="30" rows="5" required />
