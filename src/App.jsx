f31e5b57d8495cad806f47a48e679a53d100b4bf
LautaroLD <duranlautarogabriel@gmail.com>
2023-11-05 21:02:56 -0300

    routes update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import React, { lazy, Suspense } from 'react';
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Route, Routes, BrowserRouter, useNavigate, Navigate } from 'react-router-dom';
 import Loading from './assets/Spinner-loading.svg'
-
 const HomeScreen = lazy(() => import('./screens/HomeScreen'))
 const MessageSendScreen = lazy(() => import('./screens/MessageSendScreen'))
 function App() {
+	// const navigate = useNavigate()
 	return (
 		<Suspense fallback={<img src={Loading} width={250} height={250} className='m-auto' />}>
 			<BrowserRouter>
 				<Routes>
-					<Route path='/' element={<HomeScreen />} />
-					<Route path='/send' element={<MessageSendScreen />} />
+					<Route exact path='/' element={<HomeScreen />} />
+					<Route exact path='/send' element={<MessageSendScreen />} />
+					<Route path="*" element={<Navigate to='/' />} />
 				</Routes>
 			</BrowserRouter>
 		</Suspense>
