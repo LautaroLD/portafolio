d63aa656445379bf718adb067563b208eb0f4577
LautaroLD <duranlautarogabriel@gmail.com>
2023-11-05 21:42:38 -0300

    routes update

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,9 +4,7 @@ import { useNavigate } from 'react-router-dom'
 function Form() {
     const navigate = useNavigate()
     return (
-        <form onSubmit={() => {
-            navigate('/sendMessage')
-        }} className='form' name='form-contacto' netlify>
+        <form className='form' name='form-contacto' netlify="true">
             <input className='input' placeholder='Nombre' type="text" name="name" required />
             <input className='input' placeholder='Email' type="email" name="email" required />
             <textarea className='input' placeholder='Mensaje' name="content" cols="30" rows="5" required />
