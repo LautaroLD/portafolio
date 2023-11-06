8c185be14e33ac2e65cc2501a1354b026fc01910
Lautaro Durán <84945392+LautaroLD@users.noreply.github.com>
2023-11-06 11:29:06 -0300

    Actualizar Form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,9 +2,8 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
 function Form() {
-    const navigate = useNavigate()
     return (
-        <form className='form' name='form-contacto' data-netlify="true">
+        <form className='form' name='contacto' method="POST" data-netlify="true">
             <input className='input' placeholder='Nombre' type="text" name="name" required />
             <input className='input' placeholder='Email' type="email" name="email" required />
             <textarea className='input' placeholder='Mensaje' name="content" cols="30" rows="5" required />
