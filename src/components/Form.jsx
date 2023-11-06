fbf12a611bfa77d8a5bca4d963428941cd7d71c9
LautaroLD <duranlautarogabriel@gmail.com>
2023-11-06 14:33:16 -0300

    form update

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,8 +3,8 @@ import { useNavigate } from 'react-router-dom'
 
 function Form() {
     return (
-        <form className='form' name='contacto' method="POST" data-netlify="true">
-            <input className='input' placeholder='Nombre' type="text" name="name" required />
+        <form className='form' name='contacto' method="POST" data-netlify="true" netlify>
+            <input className='input' placeholder='Nombre' type="text" name="sender-name" required />
             <input className='input' placeholder='Email' type="email" name="email" required />
             <textarea className='input' placeholder='Mensaje' name="content" cols="30" rows="5" required />
             <button type="submit">Enviar</button>
