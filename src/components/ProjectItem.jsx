eeb16d117d1fac3acd9e754924ede2fd775617f7
LautaroLD <duranlautarogabriel@gmail.com>
2024-01-20 19:55:56 -0300

    fix projectItem

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -17,7 +17,7 @@ function ProjectItem() {
                         <p className='text-s-text m-auto leading-loose'>{item[1].info}</p>
                         <div className='flex justify-around items-center pt-3'>
                             {
-                                 mobile[item[1].name] && <a href={item[1].url} target='_blank' className=' rounded-xl text-s-text font-w-b py-2 px-4 decoration-0 bg-btn-bg text-light hover:bg-btn-bg-hover hover:scale-110 duration-300' rel="noreferrer" >Visitar</a>
+                                !mobile.includes(item[1].name) && <a href={item[1].url} target='_blank' className=' rounded-xl text-s-text font-w-b py-2 px-4 decoration-0 bg-btn-bg text-light hover:bg-btn-bg-hover hover:scale-110 duration-300' rel="noreferrer" >Visitar</a>
                             }
                             <a href={item[1].repository} target='_blank' className='rounded-xl text-s-text font-w-b py-2 px-4 decoration-0 text-gray hover:scale-110 duration-300 hover:text-btn-bg-hover' rel="noreferrer">Repositorio</a>
                         </div>
