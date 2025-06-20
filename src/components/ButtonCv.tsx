94ef33792956f0be644dbf233a924c112324a19a
Lautaro Durán <84945392+LautaroLD@users.noreply.github.com>
2025-06-20 00:48:37 -0300

    Actualizar ButtonCv.tsx

--- src/components/ButtonCv.tsx
+++ src/components/ButtonCv.tsx
@@ -4,7 +4,7 @@ export default function ButtonCv() {
   return (
     <Link
       className='text-dark bg-light text-s-text text-center font-w-b m-auto py-3 px-8 decoration-0 transition-all duration-500 border w-full sm:w-fit sm:mr-0 hover:bg-dark hover:text-light animate-pulse'
-      to='https://drive.google.com/file/d/1pZ0UjisrhoWm_nbWVIbczqlQdN11RiSj/view?usp=share_link'
+      to='https://drive.google.com/file/d/1AhQq2FOn-JL3xwNmSreW7IWah7KnRpT0/view?usp=drivesdk'
       target='_blank'
       rel='noreferrer'
     >
